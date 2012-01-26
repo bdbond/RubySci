8663f00e0fe44dc7772f32cae614c7402973069f
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2012-01-25 22:34:18 -0500

    Finished select

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -1,6 +1,21 @@
 module DatasetsHelper
 
   def options_materials
-    
+    str = '<option>????</option>'
+    str << '<option>Aluminum</option>'
+    str << '<option>Copper</option>'
+    str << '<option>Graphite</option>'
+    str << '<option>Stainless Steel</option>'
+    str
   end
+
+  def options_geometries
+    str = '<option>????</option>'
+    str << '<option>Cone</option>'
+    str << '<option>Disc</option>'
+    str << '<option>Sphere</option>'
+    str << '<option>Point</option>'
+    str
+  end
+
 end
